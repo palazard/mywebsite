9cf611623255ebd168a67a1220628e6bf3af6f77
palazard <alazard.pauline@gmail.com>
2023-06-01 12:14:37 +0800

    menu link scrolling with react-scroll

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,13 +3,15 @@ import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Nav from 'react-bootstrap/Nav';
 import './Sidebar.css';
+import { Link} from 'react-scroll';
+
 
 function Sidebar({name}) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
+  
   return (
     <>
       <div className="custom-nav-bar">
@@ -21,15 +23,27 @@ function Sidebar({name}) {
 
       <Offcanvas show={show} onHide={handleClose} responsive="xl">
         <Offcanvas.Body className="custom-vertical">
-          <Offcanvas.Title>PAULINE ALAZARD</Offcanvas.Title>
+          <Offcanvas.Title>{name}</Offcanvas.Title>
             <Nav className="mr-auto custom-vertical custom-nav">
+              <Link as='Nav.Link' to="home" spy={true} smooth={true} duration={500} offset={-50} onClick={handleClose}>
                 <Nav.Link href="#home">Home</Nav.Link>
+              </Link>
+              <Link to="about" spy={true} smooth={true} duration={500} offset={-50} onClick={handleClose}>
                 <Nav.Link href="#about">About Me</Nav.Link>
-                <Nav.Link href="#portfolio">Skills</Nav.Link>
-                <Nav.Link href="#portfolio">Coding Projects</Nav.Link>
-                <Nav.Link href="#portfolio">CMS Websites</Nav.Link>
+              </Link>
+              <Link to="skills" spy={true} smooth={true} duration={500} offset={-50} onClick={handleClose}>
+                <Nav.Link href="#skills">Skills</Nav.Link>
+              </Link>
+              <Link to="coding" spy={true} smooth={true} duration={500} offset={-50} onClick={handleClose}>
+                <Nav.Link href="#coding">Coding Projects</Nav.Link>
+              </Link>
+              <Link to="cms" spy={true} smooth={true} duration={500} offset={-50} onClick={handleClose}>
+                <Nav.Link href="#cms">CMS Websites</Nav.Link>
+              </Link>
+              <Link to="contact" spy={true} smooth={true} duration={500} offset={-50} onClick={handleClose}>
                 <Nav.Link href="#contact">Contact</Nav.Link>
-                <div className='custom-buffer'></div>
+              </Link>
+              <div className='custom-buffer'></div>
             </Nav>
         </Offcanvas.Body>
       </Offcanvas>
--- src/container/App.js
+++ src/container/App.js
@@ -8,20 +8,34 @@ import Cms from '../components/Cms/Cms';
 import './App.css';
 import Contact from '../components/Contact/Contact';
 import data from "../contentList.json";
+import { Element } from 'react-scroll';
 
 
 
 function App() {
   return (
     <div >
-      <Sidebar name={data.introduction.name}/>
+      <Sidebar name={data.introduction[0].name}/>
       <Container fluid="xxl" className="custom-main-container">
-        <Introduction data={data.introduction}/>
-        <About title="About me" dataAbout={data.about} data={data.education}/>
-        <Skills title="Skills" dataTechnologies={data.technologies} dataHumanSkills={data.human} dataLanguages={data.languages}/>
-        <Projects title="Coding Projects" data={data.projects}/>
-        <Cms title="CMS Websites" data={data.cms}/>
-        <Contact title="Contact me" data={data.contact}/>
+        <Element name="home" >
+          <Introduction data={data.introduction}/>
+        </Element>
+        <Element name="about" >
+          <About title="About me" dataAbout={data.about} data={data.education}/>
+        </Element>
+        <Element name="skills" >
+          <Skills title="Skills" dataTechnologies={data.technologies} dataHumanSkills={data.human} dataLanguages={data.languages}/>
+        </Element>
+        <Element name="coding" >
+          <Projects title="Coding Projects" data={data.projects}/>
+        </Element>
+        <Element name="cms" >
+          <Cms title="CMS Websites" data={data.cms}/>
+        </Element>
+        <Element name="contact" >
+          <Contact title="Contact me" data={data.contact}/>
+        </Element>
+        
       </Container>
     </div>
   );
